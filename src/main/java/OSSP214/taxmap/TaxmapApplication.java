f2cd866962a5b3b65bd1564b36b61f15fedc69ea
KimunPark <qkrrlans00@dgu.ac.kr>
2024-04-01 13:57:00 +0900

    created spring boot project

---
+++ src/main/java/OSSP214/taxmap/TaxmapApplication.java
@@ -0,0 +1,13 @@
+package OSSP214.taxmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxmapApplication.class, args);
+	}
+
+}
