3caf70b95067b5cb7a1d85fd59768ddc5281c98d
Ryeonasd <qkrrlans00@naver.com>
2024-04-16 18:01:45 +0900

    implemented OrganizationInfo generation on startup; potential performance issue

--- src/main/java/OSSP214/taxmap/services/OrganizationInfoService.java
+++ src/main/java/OSSP214/taxmap/services/OrganizationInfoService.java
@@ -1,7 +1,11 @@
 package OSSP214.taxmap.services;
 
 import OSSP214.taxmap.models.OrganizationInfo;
+import OSSP214.taxmap.models.SubsidyInfo;
 import OSSP214.taxmap.repositories.OrganizationInfoRepository;
+import OSSP214.taxmap.repositories.SubsidyInfoRepository;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +15,11 @@ import java.util.Optional;
 public class OrganizationInfoService {
 
     private final OrganizationInfoRepository organizationInfoRepository;
+    private final SubsidyInfoRepository subsidyInfoRepository;
 
-    public OrganizationInfoService(OrganizationInfoRepository organizationInfoRepository) {
+    public OrganizationInfoService(OrganizationInfoRepository organizationInfoRepository, SubsidyInfoRepository subsidyInfoRepository) {
         this.organizationInfoRepository = organizationInfoRepository;
+        this.subsidyInfoRepository = subsidyInfoRepository;
     }
 
     public List<OrganizationInfo> getAll() {
@@ -22,5 +28,25 @@ public class OrganizationInfoService {
 
     public Optional<OrganizationInfo> getById(Long id) {
         return organizationInfoRepository.findById(id);
+    }
+
+    // bean 생성과 서버 시작 사이에 호출되는 메서드
+    // 데이터 400개 기준 7.5초정도 걸림
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void init() {
+        List<SubsidyInfo> subsidyInfos = subsidyInfoRepository.findAll();
+        for (SubsidyInfo subsidyInfo : subsidyInfos) {
+            if (organizationInfoRepository.findByOrgName(subsidyInfo.getOrgName()).isEmpty()) {
+                OrganizationInfo newOrganizationInfo = OrganizationInfo.builder()
+                        .orgName(subsidyInfo.getOrgName())
+                        .representativeName(subsidyInfo.getRepresentativeName())
+                        .phoneNumber(subsidyInfo.getPhoneNumber())
+                        .address(subsidyInfo.getAddress())
+                        .build();
+
+                organizationInfoRepository.save(newOrganizationInfo);
+            }
+        }
     }
 }
