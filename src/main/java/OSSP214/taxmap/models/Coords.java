b7333524162752cf643df052852c5ba7e0afa6a6
Ryeonasd <qkrrlans00@naver.com>
2024-04-17 18:53:41 +0900

    added comments

--- src/main/java/OSSP214/taxmap/models/Coords.java
+++ src/main/java/OSSP214/taxmap/models/Coords.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import java.util.List;
 
+// 마커 정보
 @Entity
 @Getter
 @Setter
--- src/main/java/OSSP214/taxmap/models/ViewRange.java
+++ src/main/java/OSSP214/taxmap/models/ViewRange.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ViewRange {
-    private float min_lng;
-    private float max_lng;
-    private float min_lat;
-    private float max_lat;
+    private double minLat;
+    private double maxLat;
+    private double minLng;
+    private double maxLng;
 }
