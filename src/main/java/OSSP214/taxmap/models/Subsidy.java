c05d374704a593ea339bacd982eda0251d76efe4
Ryeonasd <qkrrlans00@naver.com>
2024-04-27 20:23:01 +0900

    rebase

--- src/main/java/OSSP214/taxmap/models/Subsidy.java
+++ src/main/java/OSSP214/taxmap/models/Subsidy.java
@@ -1,6 +1,5 @@
 package OSSP214.taxmap.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.poiji.annotation.ExcelCell;
 import jakarta.persistence.*;
 import lombok.*;
@@ -34,7 +33,6 @@ public class Subsidy {
 
     @ManyToOne
     @JoinColumn
-    @JsonIgnore
     private Organization orgInfo;
 
     @ExcelCell(5)
