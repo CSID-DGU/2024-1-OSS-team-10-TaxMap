42fa29e288b0384ca9daacec4a67d27fa83a6d39
Ryeonasd <qkrrlans00@naver.com>
2024-05-11 15:30:28 +0900

    added endpoint to get govOffice list from subsidy information

--- src/main/java/OSSP214/taxmap/controllers/SubsidyController.java
+++ src/main/java/OSSP214/taxmap/controllers/SubsidyController.java
@@ -3,6 +3,7 @@ package OSSP214.taxmap.controllers;
 import OSSP214.taxmap.models.Subsidy;
 import OSSP214.taxmap.models.SubsidyDTO;
 import OSSP214.taxmap.services.SubsidyService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,15 @@ public class SubsidyController {
         this.subsidyService = subsidyService;
     }
 
+    // 보조금 정보에 존재하는 중앙부처 list 받아오기
+    @GetMapping(path = "/govofficelist")
+    public List<String> govOfficeList() {
+        return subsidyService.getGovOfficeList();
+    }
+
 
     // 모든 보조금 검색
-    @RequestMapping(path = "/all")
+    @GetMapping(path = "/all")
     public List<SubsidyDTO> all() {
         List<Subsidy> subsidies = subsidyService.getAll();
         return subsidies.stream()
@@ -29,7 +36,7 @@ public class SubsidyController {
     }
 
     // 보조금 id로 검색
-    @RequestMapping(path = "/{id}")
+    @GetMapping(path = "/{id}")
     public Subsidy one(@PathVariable Long id) {
         return subsidyService.getById(id).orElseThrow();
     }
