b709dbdbbf843abedeecba1ed07523db491eb498
geopark021 <geobrown021@gmail.com>
2024-06-02 02:39:20 +0900

    feat: 하나의 마커에 총 사업비 금액대별로 다른 마커 이미지 적용

--- src/components/MapContent/MapContent.jsx
+++ src/components/MapContent/MapContent.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./MapContent.css";
 import Sidebar from "../Sidebar/Sidebar";
+import clicked_largest_icon from "../../assets/images/markers/marker_icon_largest_clicked.png";
+import clicked_middle_icon from "../../assets/images/markers/marker_icon_middle_clicked.png";
+import clicked_low_icon from "../../assets/images/markers/marker_icon_low_clicked.png";
+import largest_icon from "../../assets/images/markers/marker_icon_largest.png";
+import middle_icon from "../../assets/images/markers/marker_icon_middle.png";
+import low_icon from "../../assets/images/markers/marker_icon_low.png";
 
 function MapContent({ coordinates }) {
   const mapContainerRef = useRef(null);
   const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
+  const [clickMarker, setClickMarker] = useState(null);
 
   const closeSidebar = () => {
     setSelectedMarker(null);
+    setClickMarker(null);
   };
 
   useEffect(() => {
@@ -50,8 +58,35 @@ function MapContent({ coordinates }) {
             );
             if (response.ok) {
               const data = await response.json();
-              setMarkers(data);
-              data.forEach((markerData) => {
+
+              // 동일 주소의 총 사업비 합산
+              const markerMap = {};
+              data.forEach((marker) => {
+                if (!markerMap[marker.address]) {
+                  markerMap[marker.address] = {
+                    ...marker,
+                    totalExpense: 0,
+                  };
+                }
+                marker.organizations.forEach((org) => {
+                  markerMap[marker.address].totalExpense +=
+                    org.totalReceivedSubsidy;
+                });
+              });
+
+              const expenses = Object.values(markerMap);
+              setMarkers(expenses);
+
+              expenses.forEach((markerData) => {
+                let markerImage;
+                if (markerData.totalExpense >= 1500000000) {
+                  markerImage = largest_icon;
+                } else if (markerData.totalExpense >= 70000000) {
+                  markerImage = middle_icon;
+                } else {
+                  markerImage = low_icon;
+                }
+
                 const marker = new window.kakao.maps.Marker({
                   map: map,
                   position: new window.kakao.maps.LatLng(
@@ -59,41 +94,111 @@ function MapContent({ coordinates }) {
                     markerData.longitude
                   ),
                   title: markerData.address,
+                  image: new window.kakao.maps.MarkerImage(
+                    markerImage,
+                    new window.kakao.maps.Size(24, 35)
+                  ),
                 });
 
                 window.kakao.maps.event.addListener(
                   marker,
                   "click",
                   async () => {
-                    console.log("Marker Data:", markerData);
+                    if (clickMarker) {
+                      const { marker: prevMarker, data: prevData } =
+                        clickMarker;
+                      let previousMarkerImage;
+                      if (prevData.totalExpense >= 1500000000) {
+                        previousMarkerImage = largest_icon;
+                      } else if (prevData.totalExpense >= 100000000) {
+                        previousMarkerImage = middle_icon;
+                      } else {
+                        previousMarkerImage = low_icon;
+                      }
+                      prevMarker.setImage(
+                        new window.kakao.maps.MarkerImage(
+                          previousMarkerImage,
+                          new window.kakao.maps.Size(24, 35)
+                        )
+                      );
+                    }
 
-                    const orgSubsidies = await Promise.all(
-                      markerData.organizations.map(async (org) => {
-                        const subsidyPromises = org.subsidies.map(
-                          async (sub) => {
-                            const subsidyResponse = await fetch(
-                              `${process.env.REACT_APP_API_URL}/api/subsidy/${sub.id}`
-                            );
-                            if (subsidyResponse.ok) {
-                              return await subsidyResponse.json();
-                            } else {
-                              throw new Error(
-                                "Failed to fetch subsidy details"
-                              );
-                            }
-                          }
-                        );
-                        return {
-                          ...org,
-                          subsidies: await Promise.all(subsidyPromises),
-                        };
-                      })
+                    let clickedMarkerImage;
+                    if (markerData.totalExpense >= 100000000) {
+                      clickedMarkerImage = clicked_largest_icon;
+                    } else if (markerData.totalExpense >= 10000000) {
+                      clickedMarkerImage = clicked_middle_icon;
+                    } else {
+                      clickedMarkerImage = clicked_low_icon;
+                    }
+
+                    marker.setImage(
+                      new window.kakao.maps.MarkerImage(
+                        clickedMarkerImage,
+                        new window.kakao.maps.Size(24, 35)
+                      )
                     );
 
-                    setSelectedMarker({
-                      ...markerData,
-                      organizations: orgSubsidies,
-                    });
+                    if (clickMarker && clickMarker.marker === marker) {
+                      marker.setImage(
+                        new window.kakao.maps.MarkerImage(
+                          markerImage,
+                          new window.kakao.maps.Size(24, 35)
+                        )
+                      );
+                      setClickMarker(null);
+                      return;
+                    }
+
+                    setClickMarker({ marker, data: markerData });
+
+                    console.log("Marker Data:", markerData);
+
+                    try {
+                      const orgSubsidies = await Promise.all(
+                        markerData.organizations.map(async (org) => {
+                          const subsidyPromises = org.subsidies.map(
+                            async (sub) => {
+                              try {
+                                const subsidyResponse = await fetch(
+                                  `${process.env.REACT_APP_API_URL}/api/subsidy/${sub.id}`
+                                );
+                                if (subsidyResponse.ok) {
+                                  return await subsidyResponse.json();
+                                } else {
+                                  console.error(
+                                    `Failed to fetch subsidy details for subsidy ID: ${sub.id}`
+                                  );
+                                  return null;
+                                }
+                              } catch (subError) {
+                                console.error(
+                                  `Error fetching subsidy details for subsidy ID: ${sub.id}`,
+                                  subError
+                                );
+                                return null;
+                              }
+                            }
+                          );
+                          return {
+                            ...org,
+                            subsidies: await Promise.all(subsidyPromises).then(
+                              (results) => results.filter((res) => res !== null)
+                            ),
+                          };
+                        })
+                      );
+
+                      setSelectedMarker({
+                        ...markerData,
+                        organizations: orgSubsidies,
+                      });
+                    } catch (orgError) {
+                      console.error(
+                        "Error fetching organizations subsidies details:",
+                        orgError
+                      );
+                    }
                   }
                 );
               });
